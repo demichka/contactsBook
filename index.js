1d567404794510d27011d88049139bd023085b60
Eugenia <demichka@gmail.com>
2019-10-03 16:13:32 +0200

    added markup buttons -edit, -remove

--- index.js
+++ index.js
@@ -4,10 +4,5 @@ const sass = require('node-sass');
 const app = express();
 
 
-sass.render({
-    file: 'css/main.scss'
-  }, function(err, result) { 'css/main.css'});
-
-
 app.use(express.static('public'));
 app.listen(3003,() => console.log('Welcome to ContactsBook on the port 3003'));
